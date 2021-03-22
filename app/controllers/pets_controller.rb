d57b00ad27adaa46415d7b27091a1b8bf28afba8
Luis Castillo <raul@luiss-mbp.myfiosgateway.com>
2021-03-21 22:01:09 -0400

    commit tree c1e0336be218b0e0a959dafd7497f3a2f07e6061

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,22 +5,42 @@ class PetsController < ApplicationController
     erb :'/pets/index' 
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owner = Owner.all 
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pet = Pet.create(params[:pet])
+    if !params['owner']['name'].empty?
+      @pet.update(owner: Owner.create(params[:owner])) 
+    end
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owner = Owner.all
+    erb :'/pets/edit' 
+  end
+
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
   patch '/pets/:id' do 
+    if !params[:pet].keys.include?("owner_id")
+      params['pet']["owner_id"] = []
+    end
+      
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
 
+    if !params["owner"]["name"].empty?
+      @pet.update(owner: Owner.create(params[:owner]))
+    end
+    
     redirect to "pets/#{@pet.id}"
   end
 end
